1b2247c3291adee35699c5cee111af4f41879d89
Hariom Joshi <goodhariom@gmail.com>
2025-03-07 03:41:41 +0530

    Delete Backend/dist directory

--- Backend/dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-const app = express();
-dotenv.config();
-app.use(cors());
-app.listen(process.env.PORT, () => {
-    console.log("Server started on Port: ", process.env.PORT);
-});
